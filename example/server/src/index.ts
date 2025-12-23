e28f39de3678c4b74767f1da3370aea5437f3755
kedom <kedom@vmcall.net>
2025-12-23 12:24:48 +0100

    wip: improve tsconfig and path param types

--- example/server/src/index.ts
+++ example/server/src/index.ts
@@ -2,6 +2,7 @@ import { serve } from '@hono/node-server'
 import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
 import { z } from 'zod'
 
 const posts = [
@@ -13,21 +14,29 @@ const posts = [
 let nextId = 4
 
 const app = new Hono()
-
-app.use('/*', cors())
+app.use(cors(), logger())
 
 const routes = app
   .get('/posts', (c) => {
     return c.json(posts)
   })
-  .get('/posts/:id', zValidator('param', z.object({ id: z.string() })), (c) => {
-    const { id } = c.req.valid('param')
-    const post = posts.find((p) => p.id === parseInt(id))
-    if (!post) {
-      return c.json({ error: 'Post not found' }, 404)
+  .get(
+    '/posts/:id',
+    zValidator(
+      'param',
+      z.object({
+        id: z.coerce.number(),
+      })
+    ),
+    (c) => {
+      const { id } = c.req.valid('param')
+      const post = posts.find((p) => p.id === id)
+      if (!post) {
+        return c.notFound()
+      }
+      return c.json(post)
     }
-    return c.json(post)
-  })
+  )
   .post(
     '/posts',
     zValidator(
@@ -46,12 +55,17 @@ const routes = app
   )
   .delete(
     '/posts/:id',
-    zValidator('param', z.object({ id: z.string() })),
+    zValidator(
+      'param',
+      z.object({
+        id: z.coerce.number(),
+      })
+    ),
     (c) => {
       const { id } = c.req.valid('param')
-      const index = posts.findIndex((p) => p.id === parseInt(id))
+      const index = posts.findIndex((p) => p.id === id)
       if (index === -1) {
-        return c.json({ error: 'Post not found' }, 404)
+        return c.notFound()
       }
       const deleted = posts.splice(index, 1)[0]
       return c.json(deleted)
